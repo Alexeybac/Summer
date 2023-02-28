2393f0e7d46ae321e6c3be3e78b3114f1b3f89ae
Лёха <Alexbac@yandex.ru>
2023-02-28 20:33:56 +0300

    summ1

--- summ1/Program.cs
+++ summ1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Добрый день!");
+Console.Write("Введите пожалуйста первое число:  ");
+double a = double.Parse(Console.ReadLine());
+
+
+Console.Write("Введите пожалуйста второе число:  ");
+double b = double.Parse(Console.ReadLine());
+
+
+double c = a + b;
+
+Console.Write("Сумма чисел Вам введенный составляет:  ");
+Console.WriteLine(c);
