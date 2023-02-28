ec3c0fd7f93a4759929dd2b93a8eabaaca6b6711
Лёха <Alexbac@yandex.ru>
2023-02-28 21:29:15 +0300

    Максимум из 5ти цифр

---
+++ giri/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int a = 1345;
+int b = 3;
+int c = 8;
+int d = 122;
+int e = 6;
+
+int max = a;
+
+if (max < a) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+Console.WriteLine(max);
