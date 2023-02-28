80061f7e355687c5037ba38529a0a27b0b0dd6d6
Лёха <Alexbac@yandex.ru>
2023-02-28 21:20:54 +0300

    Привет, Танька

---
+++ Hi/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ведите, пожалуйста имя: - ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "таня")
+{
+    Console.WriteLine("О! Какие лююююди))). Привет, Танюшка!");
+}
+else
+{
+    Console.Write("А, это ты. Ну, привет ");
+    Console.WriteLine(username);
+}
