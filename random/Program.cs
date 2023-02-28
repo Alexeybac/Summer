9bff38723b5c7ef237442461c3d8f2b84e96f4dd
Лёха <Alexbac@yandex.ru>
2023-02-28 21:04:33 +0300

    Рандомные числа

---
+++ random/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = new Random().Next(1, 10);
+
+Console.Write("Первое рандомное число - ");
+Console.WriteLine(a);
+
+int b = new Random().Next(1, 10);
+
+Console.Write("Второе рандомное число - ");
+Console.WriteLine(b);
+
+double c = a + b;
+
+Console.Write("Сумма двух рандомных чисел составляет: - ");
+Console.WriteLine(c);
