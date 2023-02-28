f8e933fc1a497bee4cc7a3f265be5ba56033aa70
Лёха <Alexbac@yandex.ru>
2023-02-28 20:50:21 +0300

    Деление

---
+++ Div/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Добрый день!!!");
+Console.Write("Введите, пожалуйста, первое число для деления - А: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введите, пожалуйста, второе число для деления - Б: ");
+double b = double.Parse(Console.ReadLine());
+
+double c = a/b;
+
+Console.Write("Результат деления А/Б:   ");
+Console.WriteLine(c);
